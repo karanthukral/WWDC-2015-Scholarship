e56181b31eaae7784b70234cc2023f06e0251f83
Karan Thukral <karanthukral@gmail.com>
2015-04-14 23:15:28 -0400

    Initial Commit

---
+++ KaranThukralTests/KaranThukralTests.swift
@@ -0,0 +1,36 @@
+//
+//  KaranThukralTests.swift
+//  KaranThukralTests
+//
+//  Created by Karan Thukral on 2015-04-14.
+//  Copyright (c) 2015 Karan Thukral. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KaranThukralTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
