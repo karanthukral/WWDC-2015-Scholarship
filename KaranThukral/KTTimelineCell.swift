ff7700428467b374790cfc976b1898d71e3d5a0b
Karan Thukral <karanthukral@gmail.com>
2015-04-23 18:42:25 -0400

    Fixed image view bug

--- KaranThukral/KTTimelineCell.swift
+++ KaranThukral/KTTimelineCell.swift
@@ -11,18 +11,8 @@ import UIKit
 class KTTimelineCell: UITableViewCell {
 
 	@IBOutlet weak var titleLabel: UILabel!
+
 	@IBOutlet weak var selectionImageView: UIImageView!
-	
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 	
 	func setUp(forStory story: KTStory, isSelected selected: Bool) {
 		self.titleLabel.text = story.title
